9068978e6ffbb99f43d1af73bafcf80531ec49c1
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-03-04 00:45:47 +0530

    .evn file created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
 
 export default function Index() {
   return (
-    <View>
-      <Text>This is Index Page</Text>
-      {/* <Redirect href={'/login/logIn'} /> */}
+    <View
+      style={styles.container}
+    >
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 25,
+    height: '100%',
+  }
+});
